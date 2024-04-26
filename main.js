1c923be38a783f242543ec3c71f803f03033c5e5
Luc4sac <110491837+Luc4sac@users.noreply.github.com>
2024-04-26 09:27:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const botoes = document.querySelector(".botao");
 
 for (let i=0;i<botoes.lenght;i++){
     botoes[i].onclick=function(){
-        botoes[i].classicList.add("ativo");
+        for(j=0;j<botoes.lenght;j++){
+        botoes[j].classicList.remove("ativo");
     }
+    botoes[i].classList.add("ativo")
+}
+
 }

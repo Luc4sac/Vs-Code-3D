09ea2d5f87909a5830c1b8f95a4030c228806e3d
Luc4sac <110491837+Luc4sac@users.noreply.github.com>
2024-08-30 07:44:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+
+const perguntas = [
+    {
+        enunciado: "Você gosta da Inteligência Artificial?",
+        alternativas:["Sim","Não"]
+    },
+    {
+        enunciado: "Você acha a IA perigoso?",
+        alternativas:["Sim","Não"]
+    },
+    {
+        enunciado: "Você acredita que IA ajuda no seu cotidiano?",
+        alternativas:["Alternativa 1","Alternativa 2"]
+    }
+]
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[posicao];
+    caixaPergunta.textContent = perguntaAtual.enunciado;
+    mostraAlternativa();
+}
+function mostraAlternativa(){
+    for(const alternativa)
+}
